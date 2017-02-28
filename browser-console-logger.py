f735213f6afdf6e0f72272b2875db7f9502e60f6
Alex Scherbanov <alex@egotv.ru>
2017-02-28 19:03:36 +0300

    add the py

---
+++ browser-console-logger.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+from flask import request
+import logging
+import os
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)                # = Access-Control-Allow-Origin: *
+
+
+## it's not convenient to use xcode or adb and a wire to get browser logs
+## so we just decorate console.log() to make it send a post request here containing a console message
+
+## $ curl -X POST -H "Content-Type: text/plain" http://localhost:9907/browser_console_logger --data "hello world"
+
+
+
+## lets make it not log this:
+## 172.17.0.1 - - [19/Dec/2015 18:40:45] "POST /browser_console_logger HTTP/1.1" 204 -
+log = logging.getLogger('werkzeug')   # http://stackoverflow.com/questions/14888799/disable-console-messages-in-flask-server
+log.setLevel(logging.ERROR)
+
+
+app.logger.addHandler(logging.StreamHandler())
+app.logger.setLevel(logging.INFO)
+
+
+
+### currently logs to sdtout only, (maybe later) add a rotated log file
+
+@app.route('/browser_console_logger', methods=['POST'])
+def hello_world():
+    l = request.data.decode(encoding="utf-8", errors="replace")
+    app.logger.info("--- %s" % (l))
+    return ('', 204)
+
+
+if __name__ == '__main__':
+    print(os.path.basename(__file__) + " running...")
+    app.run(host='0.0.0.0', port=9907)   # = LOGG rotated, leetspeak
+
+    
